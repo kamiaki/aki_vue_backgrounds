43fb9e8ab07d3a30d6beb4ee206a93c2f2f9f773
kamiaki <247557768@qq.com>
2020-11-07 13:35:49 +0800

    更新了 可配参数

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app" style="height: 100%">
-    <AkiBackGround1 style="height:100%;width: 100%;"
+    <div style="  height: 50%">
+      <div>我是一堆文字</div>
+      <div>我是一堆文字</div>
+      <div>我是一堆文字</div>
+      <div>我是一堆文字</div>
+    </div>
+    <AkiBackGround1 style="height:100%;width: 100%;
+    position: fixed;top: 0;left: 0;z-index: -999"
     column="4" row="3" rotate="45" scale="2" saturation="100" light="50"></AkiBackGround1>
   </div>
 </template>
